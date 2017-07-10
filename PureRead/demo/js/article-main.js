9c0f3a7357423919c32b7b07afaa2d8c3d302286
NCMT <NCMT@DESKTOP-GJPJ932>
2017-07-10 14:34:15 +0800

    完善功能

---
+++ PureRead/demo/js/article-main.js
@@ -0,0 +1,52 @@
+var btn = document.getElementById('btn-switch');
+var style = document.getElementById("style-css");
+var btnSun = document.getElementById("switch-sun");
+var btnMoon = document.getElementById("switch-moon");
+btn.onclick = function() {
+    if(style.getAttribute("href") === "css/style/style-dark.css") {
+        style.setAttribute("href", "css/style/style-light.css");
+        btnSun.style.display = "none";
+        btnMoon.style.display = "inline-block";
+    } else {
+        style.setAttribute("href", "css/style/style-dark.css");
+        btnMoon.style.display = "none";
+        btnSun.style.display = "inline-block";
+    }
+
+}
+
+function goTop(acceleration, time) {
+    acceleration = acceleration || 0.1;
+    time = time || 20;
+    var x1 = 0;
+    var y1 = 0;
+    var x2 = 0;
+    var y2 = 0;
+    var x3 = 0;
+    var y3 = 0;
+    if(document.documentElement) {
+        x1 = document.documentElement.scrollLeft || 0;
+        y1 = document.documentElement.scrollTop || 0;
+    }
+    if(document.body) {
+        x2 = document.body.scrollLeft || 0;
+        y2 = document.body.scrollTop || 0;
+    }
+    var x3 = window.scrollX || 0;
+    var y3 = window.scrollY || 0;
+
+    // 滚动条到页面顶部的水平距离 
+    var x = Math.max(x1, Math.max(x2, x3));
+    // 滚动条到页面顶部的垂直距离 
+    var y = Math.max(y1, Math.max(y2, y3));
+
+    // 滚动距离 = 目前距离 / 速度, 因为距离原来越小, 速度是大于 1 的数, 所以滚动距离会越来越小 
+    var speed = 1 + acceleration;
+    window.scrollTo(Math.floor(x / speed), Math.floor(y / speed));
+
+    // 如果距离不为零, 继续调用迭代本函数 
+    if(x > 0 || y > 0) {
+        var invokeFunction = "goTop(" + acceleration + ", " + time + ")";
+        window.setTimeout(invokeFunction, time);
+    }
+}
